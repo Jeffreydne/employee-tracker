228ce67dd884a2a1a8b8523ed68873870adca7c6
Jeffreydne <Jeffreydne@gmail.com>
2024-01-25 13:29:20 -0800

    add structure to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- Department table
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 -- Role table
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
 -- employee table
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30), NOT NULL
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+-- seed departments table
+INSERT INTO departments (department_name)
+VALUES (""),
+       (""),
+       (""),
+       ("");
+-- seed roles table
+INSERT INTO roles (title, salary, department_id)
+VALUES ("", , ),
+       ("", , ),
+       ("", , ),
+       ("", , );
+-- seed employees table
+       INSERT INTO departments (first_name, last_name, role_id, manager_id)
+VALUES ("", "",  , ),
+       ("", "",  , ),
+       ("", "",  , ),
+       ("", "",  , );
