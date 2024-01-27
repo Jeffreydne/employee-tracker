b1d299e1b54d34cda74c3f3a869e31b5b13dcc57
Jeffreydne <Jeffreydne@gmail.com>
2024-01-27 13:10:59 -0800

    change id for cust and role to c_id 7 r_id. Fix role table

--- db/schema.sql
+++ db/schema.sql
@@ -10,14 +10,14 @@ CREATE TABLE departments (
 );
 -- Role table
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    r_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
 -- employee table
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    c_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
